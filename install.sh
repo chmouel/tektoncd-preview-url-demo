8e9a8ba05fa2987b865ad2873e90945e40ea2141
Chmouel Boudjnah <chmouel@chmouel.com>
2020-01-20 19:39:57 +0100

    new stuff
    Signed-off-by: Chmouel Boudjnah <chmouel@chmouel.com>

--- install.sh
+++ install.sh
@@ -21,7 +21,16 @@ oc project ${TARGET_NAMESPACE} 2>/dev/null >/dev/null || {
 }
 
 
-for file in *.yaml;do
+for task in buildah/buildah  openshift-client/openshift-client-task;do
+            curl -Ls -f https://raw.githubusercontent.com/tektoncd/catalog/master/${task}.yaml | ${K} apply -f -
+done
+
+oc adm policy add-scc-to-user privileged -z ci-openshift-triggers-sa
+for role in image-builder deployer;do
+    oc policy add-role-to-user system:${role} -z ci-openshift-triggers-sa
+done
+
+for file in templates/*.yaml;do
     kubectl delete -f  ${file} 2>/dev/null || true
     kubectl create -f ${file}
 done
